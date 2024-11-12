d8f0371314a07b3d76a0a372b9f3d5f0a37d1acb
BoxMaku <maximiliano.martinez@outlook.com>
2024-11-12 08:59:09 -0700

    Ep 9 Collision Detection

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,12 +1,31 @@
 package entities;
 
+import java.awt.Rectangle;
+
 public abstract class Entity {
 
     protected float x, y;
+    protected width, height;
+    protected Rectangle hitbox;
 
     public Entity(float x, float y) {
         this.x = x;
         this.y = y;
+        this.width = width;
+        this.height = height;
+        initHitbox();
+        
     }
-    
+    private void initHitbox(){      //HitBox Initiator.
+        hitbox = new Rectangle((int) x, (int) y, width, height);
+    }
+
+    protected void updateHitbox(){
+        hitbox.x = (int) x;
+        hitbox.y = (int) y;
+    }
+
+    public Rectangle getHitbox(){
+        return hitBox;
+    }
 }
